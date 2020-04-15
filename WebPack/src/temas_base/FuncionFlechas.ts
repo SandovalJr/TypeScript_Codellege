32089e5f5d94d9d70f09276bfed58117914fe386
SandovalJr <edubizzle7@gmail.com>
2020-04-15 09:44:59 -0500

    arrow function

--- WebPack/src/temas_base/FuncionFlechas.ts
+++ WebPack/src/temas_base/FuncionFlechas.ts
@@ -9,23 +9,37 @@
 
 Notas:
 El primer parametro no puede ser opcional 
-
+Los parametros opcionales deben escribirse al final 
 ******/
 
 // paramtros obligatorios
 
-function suma(x: number) {
-  let suma = x;
-  console.log(suma);
-}
+// function suma(x: number) {
+//   let suma = x;
+//   console.log(suma);
+// }
 
-suma(10);
+// suma(10);
 
-//Paramtros opcionales
-function opcionales(x: number, y?: boolean, ultimo: number = 10) {
-  let suma = x + ultimo;
-  y != undefined ? console.log(suma) : console.log("no se tuvo a y: " + suma);
-  //   console.log(suma);
-}
+// //Paramtros opcionales
+// function opcionales(x: number, ultimo: number = 10, y?: boolean) {
+//   let suma = x + ultimo;
+//   y != undefined ? console.log(suma) : console.log("no se tuvo a y: " + suma);
+//   //   console.log(suma);
+// }
 
-opcionales(10, true);
+// opcionales(10, 20);
+
+/*****
+Funcion de flechas
+******/
+
+const ArrowFunction = (x: number, y: number) => {
+  return x + y;
+};
+
+console.log(ArrowFunction(10, 5));
+
+/*****
+Metodos array funcion de flecha
+******/
