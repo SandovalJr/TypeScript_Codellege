8f4f87f715f54db9a619ecb8f553348b7b938efa
SandovalJr <edubizzle7@gmail.com>
2020-04-17 10:23:54 -0500

    tarea agregada

---
+++ WebPack/src/ProgramacionAsincrona/2practicaCallBacks.ts
@@ -0,0 +1,61 @@
+const Alumnos = [
+	{
+		id: 1,
+		Nombre: 'Alicia Villareal',
+		idCarrera: 3,
+	},
+	{
+		id: 2,
+		Nombre: 'Blanca Díaz',
+		idCarrera: 2,
+	},
+	{
+		id: 3,
+		Nombre: 'Daniel Palacios',
+		idCarrera: 4,
+	},
+	{
+		id: 4,
+		Nombre: 'Victor Lemus',
+		idCarrera: 1,
+	},
+	{
+		id: 5,
+		Nombre: 'Karen Sanchez',
+		idCarrera: 5,
+	},
+];
+
+const Carrera = [
+	{
+		idCarrera: 1,
+		carrera: 'Mercadotecnia',
+		idCentroEstudios: 1,
+	},
+	{
+		idCarrera: 2,
+		carrera: 'Historia',
+		idCentroEstudios: 3,
+	},
+	{
+		idCarrera: 3,
+		carrera: 'Nutricion',
+		idCentroEstudios: 2,
+	},
+	{
+		idCarrera: 4,
+		carrera: 'Tics',
+		idCentroEstudios: 1,
+	},
+];
+
+const CentroEstudios = [
+	{
+		id: 1,
+		Nombre: 'Ciencias y tecnologias',
+	},
+	{
+		id: 2,
+		Nombre: 'Salud',
+	},
+];
