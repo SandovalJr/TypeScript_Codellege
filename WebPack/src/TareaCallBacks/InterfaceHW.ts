c326738632d55396948e7082487e8e76682bee1a
SandovalJr <edubizzle7@gmail.com>
2020-04-17 10:35:11 -0500

    se agregaron instancias

---
+++ WebPack/src/TareaCallBacks/InterfaceHW.ts
@@ -0,0 +1,16 @@
+export interface Alumnos {
+    id:number,
+    Nombre:string,
+    idCarrera:number
+}
+
+export interface Carrera{
+    idCarrera:number,
+    carrera:string,
+    idCentroEstudios:number
+}
+
+export interface CentroEstudios{
+    id:number,
+    Nombre:string
+}
--- WebPack/src/TareaCallBacks/TareaCallBacks.ts
+++ WebPack/src/TareaCallBacks/TareaCallBacks.ts
@@ -1,63 +1,68 @@
-const Alumnos = [
-	{
-		id: 1,
-		Nombre: 'Alicia Villareal',
-		idCarrera: 3,
-	},
-	{
-		id: 2,
-		Nombre: 'Blanca Díaz',
-		idCarrera: 2,
-	},
-	{
-		id: 3,
-		Nombre: 'Daniel Palacios',
-		idCarrera: 4,
-	},
-	{
-		id: 4,
-		Nombre: 'Victor Lemus',
-		idCarrera: 1,
-	},
-	{
-		id: 5,
-		Nombre: 'Karen Sanchez',
-		idCarrera: 5,
-	},
+// Importamos las interfaces
+import { Alumnos } from "../TareaCallBacks/InterfaceHW";
+import { Carrera } from "../TareaCallBacks/InterfaceHW";
+import { CentroEstudios } from "../TareaCallBacks/InterfaceHW";
+
+const Alumnos: Array<Alumnos> = [
+  {
+    id: 1,
+    Nombre: "Alicia Villareal",
+    idCarrera: 3,
+  },
+  {
+    id: 2,
+    Nombre: "Blanca Díaz",
+    idCarrera: 2,
+  },
+  {
+    id: 3,
+    Nombre: "Daniel Palacios",
+    idCarrera: 4,
+  },
+  {
+    id: 4,
+    Nombre: "Victor Lemus",
+    idCarrera: 1,
+  },
+  {
+    id: 5,
+    Nombre: "Karen Sanchez",
+    idCarrera: 5,
+  },
 ];
 
-const Carrera = [
-	{
-		idCarrera: 1,
-		carrera: 'Mercadotecnia',
-		idCentroEstudios: 1,
-	},
-	{
-		idCarrera: 2,
-		carrera: 'Historia',
-		idCentroEstudios: 3,
-	},
-	{
-		idCarrera: 3,
-		carrera: 'Nutricion',
-		idCentroEstudios: 2,
-	},
-	{
-		idCarrera: 4,
-		carrera: 'Tics',
-		idCentroEstudios: 1,
-	},
+const Carrera: Array<Carrera> = [
+  {
+    idCarrera: 1,
+    carrera: "Mercadotecnia",
+    idCentroEstudios: 1,
+  },
+  {
+    idCarrera: 2,
+    carrera: "Historia",
+    idCentroEstudios: 3,
+  },
+  {
+    idCarrera: 3,
+    carrera: "Nutricion",
+    idCentroEstudios: 2,
+  },
+  {
+    idCarrera: 4,
+    carrera: "Tics",
+    idCentroEstudios: 1,
+  },
 ];
 
-const CentroEstudios = [
-	{
-		id: 1,
-		Nombre: 'Ciencias y tecnologias',
-	},
-	{
-		id: 2,
-		Nombre: 'Salud',
-	},
+const CentroEstudios:Array<CentroEstudios> = [
+  {
+    id: 1,
+    Nombre: "Ciencias y tecnologias",
+  },
+  {
+    id: 2,
+    Nombre: "Salud",
+  },
 ];
 
 /********Problemas**********
