f28395fe75f1b70cf54c2558cc0768bfae8fb1f4
SandovalJr <edubizzle7@gmail.com>
2020-04-20 23:05:47 -0500

    promesas

---
+++ WebPack/src/Promesas/promesas.ts
@@ -0,0 +1,11 @@
+
+
+/*****
+Promesas: Es un objeto el cual mandamos a pedir y puede o no tener un valor y represata si funciona o falla
+
+La promesa tiene 3 estados:
+-Pendiente: es el estado incial cuando se crea la promesa
+-Cumplido: cuando se cumple la promesa
+- Rechazado: cuando no se cumple con la promesa
+
+******/
