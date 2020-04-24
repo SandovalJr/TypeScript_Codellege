4e9e2f8f2e3f7e94a0763d7ea293085907d74290
SandovalJr <edubizzle7@gmail.com>
2020-04-24 10:01:17 -0500

    practica finalizada

--- RxJS/src/index.ts
+++ RxJS/src/index.ts
@@ -1,19 +1,75 @@
-import { Observer, fromEvent } from "rxjs";
+import { Observer, range, from } from "rxjs";
 import { displayLog } from "./utils/utlis";
-import { Empleado } from "../src/interfaces/interfaces";
-import { map, pluck, mapTo } from "rxjs/operators";
+import { filter, pluck } from "rxjs/operators";
+import { Empleado2 } from "./interfaces/interfaces";
 /*****
-OBSERVABLE USANDO Map To 
+OBSERVABLE USANDO Filter
 ******/
 
-const observer: Observer<any> = {
-  next: (value) => displayLog(`[Next]: ${value}`),
+// const observer: Observer<any> = {
+//   next: (value) => displayLog(`[Next]: ${value}`),
+//   error: (err) => console.error("[Error Observable] ", err.name),
+//   complete: () => console.warn("[Complete]"),
+// };
+
+// const src$ = range(1, 10);
+
+// src$.pipe(filter((v) => v % 2 == 1)).subscribe(observer);
+
+const empleados: Array<Empleado2> = [
+  {
+    id: 1,
+    nombre: "Raul",
+    puesto: "Leader Manager",
+    edad: 23,
+  },
+  {
+    id: 2,
+    nombre: "Ray",
+    puesto: "Developer Sr",
+    edad: 26,
+  },
+  {
+    id: 3,
+    nombre: "Eduardo",
+    puesto: "El intendente",
+    edad: 21,
+  },
+  {
+    id: 4,
+    nombre: "Manu",
+    puesto: "Developer Sr",
+    edad: 21,
+  },
+  {
+    id: 5,
+    nombre: "Paco",
+    puesto: "Developer Sr",
+    edad: 27,
+  },
+];
+
+/**************PRACTICA*******************
+1-Convertir el array a un observable 
+2- Filtrar a los empleados que son developers Sr y posteriormente a los que no lo son 
+3- filtrar a los empleados que osn mayores a 21 años
+*******************************************/
+
+// 1-Convertir el array a un observable
+
+const observerEmpleados: Observer<any> = {
+  next: (value) => displayLog(`${JSON.stringify(value)}`),
   error: (err) => console.error("[Error Observable] ", err.name),
   complete: () => console.warn("[Complete]"),
 };
 
-const keyup$ = fromEvent<KeyboardEvent>(document, "keyup");
+const obs$ = from(empleados);
+obs$
+  .pipe(filter((v) => v.puesto == "Developer Sr"))
+  .subscribe(observerEmpleados);
 
-const keyUpMapTo$ = keyup$.pipe(mapTo("tecla presionada"));
+obs$
+  .pipe(filter((v) => v.puesto != "Developer Sr"))
+  .subscribe(observerEmpleados);
 
-keyUpMapTo$.subscribe(observer);
+obs$.pipe(filter((v) => v.edad > 21)).subscribe(observerEmpleados);
--- RxJS/src/interfaces/interfaces.ts
+++ RxJS/src/interfaces/interfaces.ts
@@ -2,3 +2,12 @@ export interface Empleado {
   id: number;
   nombre: string;
 }
+
+
+
+export interface Empleado2 {
+  id: number;
+  nombre: string;
+  puesto: string;
+  edad: number;
+}
